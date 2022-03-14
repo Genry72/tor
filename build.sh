f05994b2e35afb1030610a6d81e9251e218f25d4
Valentin Semenov <valentin.semyonov@nexign.com>
2022-03-14 21:03:44 +0300

    Автостарт

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ docker rmi -f mytor
 #Билдим
 docker build -t mytor .
 #Стартуем
-docker run -it -p 8118:8118 -p 9050:9050 -p 3128:3128 --name tor -d mytor
+docker run -it -p 8118:8118 -p 9050:9050 -p 3128:3128 --name tor -d mytor --restart unless-stopped
