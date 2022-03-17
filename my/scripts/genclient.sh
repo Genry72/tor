c8ef3db8a7908fe18a7869c4cfa3a8e01e2d080e
Valentin Semenov <valentin.semyonov@nexign.com>
2022-03-17 21:30:49 +0300

    test

--- my/scripts/genclient.sh
+++ my/scripts/genclient.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 source ./functions.sh
 
+cd "/opt/Dockovpn_data"
+
+LOCKFILE=.gen
+
+# Regenerate certs only on the first start
+if [ ! -f $LOCKFILE ]; then
+
+    /usr/share/easy-rsa/easyrsa build-ca nopass << EOF
+
+EOF
+    # CA creation complete and you may now import and sign cert requests.
+    # Your new CA certificate file for publishing is at:
+    # /opt/Dockovpn_data/pki/ca.crt
+
+    /usr/share/easy-rsa/easyrsa gen-req MyReq nopass << EOF2
+
+EOF2
+    # Keypair and certificate request completed. Your files are:
+    # req: /opt/Dockovpn_data/pki/reqs/MyReq.req
+    # key: /opt/Dockovpn_data/pki/private/MyReq.key
+
+    /usr/share/easy-rsa/easyrsa sign-req server MyReq << EOF3
+yes
+EOF3
+    # Certificate created at: /opt/Dockovpn_data/pki/issued/MyReq.crt
+
+    openvpn --genkey --secret ta.key << EOF4
+yes
+EOF4
+
+    touch $LOCKFILE
+fi
+
+# Copy server keys and certificates
+cp pki/ca.crt pki/issued/MyReq.crt pki/private/MyReq.key ta.key /etc/openvpn
+
+cd "/opt/Dockovpn"
+
+# Print app version
+/opt/Dockovpn/version.sh
+
+
 CLIENT_PATH="$(createConfig)"
 CONTENT_TYPE=application/text
 FILE_NAME=client.ovpn
--- my/scripts/start.sh
+++ my/scripts/start.sh
@@ -23,46 +23,7 @@ iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 
-cd "/opt/Dockovpn_data"
 
-LOCKFILE=.gen
-
-# Regenerate certs only on the first start
-if [ ! -f $LOCKFILE ]; then
-
-    /usr/share/easy-rsa/easyrsa build-ca nopass << EOF
-
-EOF
-    # CA creation complete and you may now import and sign cert requests.
-    # Your new CA certificate file for publishing is at:
-    # /opt/Dockovpn_data/pki/ca.crt
-
-    /usr/share/easy-rsa/easyrsa gen-req MyReq nopass << EOF2
-
-EOF2
-    # Keypair and certificate request completed. Your files are:
-    # req: /opt/Dockovpn_data/pki/reqs/MyReq.req
-    # key: /opt/Dockovpn_data/pki/private/MyReq.key
-
-    /usr/share/easy-rsa/easyrsa sign-req server MyReq << EOF3
-yes
-EOF3
-    # Certificate created at: /opt/Dockovpn_data/pki/issued/MyReq.crt
-
-    openvpn --genkey --secret ta.key << EOF4
-yes
-EOF4
-
-    touch $LOCKFILE
-fi
-
-# Copy server keys and certificates
-cp pki/ca.crt pki/issued/MyReq.crt pki/private/MyReq.key ta.key /etc/openvpn
-
-cd "/opt/Dockovpn"
-
-# Print app version
-/opt/Dockovpn/version.sh
 
 # Need to feed key password
 openvpn --config /etc/openvpn/server.conf &
