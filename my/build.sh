c22344e2eacd5a5a23548aa07d546800e3223661
Valentin Semenov <valentin.semyonov@nexign.com>
2022-03-17 15:55:00 +0300

    add real ip

--- my/build.sh
+++ my/build.sh
@@ -5,4 +5,4 @@ docker rmi -f mymy
 #Билдим
 docker build -t mymy .
 #Стартуем
-docker run -it --cap-add=NET_ADMIN -p 9050:9050 --restart unless-stopped --dns=127.0.0.1 -p 1194:1194/udp -p 80:8080/tcp -e HOST_ADDR=localhost --name my -d mymy
+docker run -it --cap-add=NET_ADMIN -p 9050:9050 --restart unless-stopped --dns=127.0.0.1 -p 1194:1194/udp -p 80:8080/tcp -e HOST_ADDR=$(curl -s https://api.ipify.org) --name my -d mymy
