6321bac1aec6a8691bdd40c27e5ec5c0c4d076e3
Valentin Semenov <valentin.semyonov@nexign.com>
2022-03-17 20:35:39 +0300

    test

--- my/scripts/start.sh
+++ my/scripts/start.sh
@@ -65,7 +65,7 @@ cd "/opt/Dockovpn"
 /opt/Dockovpn/version.sh
 
 # Need to feed key password
-openvpn --config /etc/openvpn/server.conf &
+#openvpn --config /etc/openvpn/server.conf &
 
 # By some strange reason we need to do echo command to get to the next command
 echo " "
